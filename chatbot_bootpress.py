ed06ac53110cb9fbb509443a2712c7e06a758159
hemadarshiny <hemadarshiny@graduate.utm.my>
2025-01-14 15:53:27 +0800

    Add chatbot link

---
+++ chatbot_bootpress.py
@@ -0,0 +1,4 @@
+chatbot_link = "https://cdn.botpress.cloud/webchat/v2.3/shareable.html?configUrl=https://files.bpcontent.cloud/2025/01/13/16/20250113161917-XPH5N9TZ.json"
+
+# Example usage: 
+# Click the chatbot link to utilize the chatbot function 
